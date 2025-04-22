90054b247e3fdaf1ec7bb746e3ca7c7b23b1afd6
claudiobentodala <programmer.claudio@gmail.com>
2025-04-22 16:58:36 +0100

    putted comment in code FOR

--- exercises/ex004/for.c
+++ exercises/ex004/for.c
@@ -2,11 +2,10 @@
 
 int main()
 {
-    /*in i=0 you can on 0*/
-    for(int i=1;i < 4;i++)
-    {
+    //for is use to repeat Code how many time you want
+    //in i=0 you may put 0 and "i" mean INDEX
+    for(int i=1;i < 4;i++){
         printf("%d \n", i);
     }
-    /*for is use to repeat Code how many time you want*/
     return 0;
 }
