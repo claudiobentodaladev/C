18c4726b18c5aa4e35f80c47848a3c2937364b5e
claudiobentodala <programmer.claudio@gmail.com>
2025-05-11 21:53:35 +0100

    TAREFA TLP
    FEITA A TAREFA DE TLP GRUPO 2

---
+++ exercises/ex014/tlp_tarefa-conversor temperatura.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float fahrenheit(float celcius){
+	float fahrenheit = 32;
+	fahrenheit += celcius + (celcius*0.8);
+	return fahrenheit;
+}
+float kelvin(float celcius){
+	float kelvin = 273.1;
+	kelvin += celcius;
+	return kelvin;
+}
+
+int main(){
+	
+	float celcius_valor = 0.0,kelvin_,fahrenheit_;
+	
+	printf("Celcius: \n");
+	scanf("%f", &celcius_valor);
+	
+	printf("fahrenheit: %.1f\n", fahrenheit(celcius_valor));
+	printf("kelvin: %.1f \n", kelvin(celcius_valor));
+	return 0;
+}
