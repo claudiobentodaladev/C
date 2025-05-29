9eaf63ab9779f7a5b5324ee9a31a15e9e5a7816d
claudiobentodala <programmer.claudio@gmail.com>
2025-05-29 08:28:28 +0100

    Feita a prova de TLP

---
+++ exercises/ex016/prova-tlp.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+	
+	int idade[6] = {15,60,5,30,20,19};
+	float media = 0.0;
+	
+	printf("A media de : ");
+	
+	for(int i = 0;i < 6; i++){
+		printf("%i ", idade[i]);
+		media += idade[i];
+	}
+	printf("\n");
+	media /= 6;
+	
+	printf("A media: %f", media);
+	
+	return 0;
+}
