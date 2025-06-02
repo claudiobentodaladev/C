d9bdfd9dea02b94309cc593403f7fa37b70ca234
claudiobentodala <programmer.claudio@gmail.com>
2025-06-02 10:40:08 +0100

    deleted variable adress

--- exercises/ex002/input&output.c
+++ exercises/ex002/input&output.c
@@ -8,13 +8,13 @@ int main()
     float numberfloat = 0.0;
     
     printf("Input any Number:\n");
-    scanf("%d", &number);// %d is used to intire number
+    scanf("%d", number);// %d is used to intire number
     
     printf("Input any string:\n");
-    scanf("%s", &string);// %s is used to string
+    scanf("%s", string);// %s is used to string
     
     printf("Input any float number:\n");
-    scanf("%f", &numberfloat);// %f is used to float number
+    scanf("%f", numberfloat);// %f is used to float number
     
     printf("Output number: %d \n",number);
     printf("Output string: %s \n", string);
